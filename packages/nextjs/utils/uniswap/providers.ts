86a4dd89430aa7a75111198900c80f6c226559fc
Hunter Chang <716376+ChangoMan@users.noreply.github.com>
2024-11-27 11:45:27 -0700

    remove uniswap

--- packages/nextjs/utils/uniswap/providers.ts
+++
@@ -1,8 +0,0 @@
-import { CurrentConfig } from "./config";
-import { ethers, providers } from "ethers";
-
-// Provider Functions
-
-export function getProvider(): providers.Provider {
-  return new ethers.providers.JsonRpcProvider(CurrentConfig.rpc.base);
-}
